6900042e34b2524ae1dd1079122fc851343b0aa4
JianingYao <jianingyao619@gmail.com>
2023-02-13 15:52:12 -0500

    fix bash file

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -3,11 +3,5 @@
 for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
-  sed -i "$linenum i This is the $commit_count th sentence." foo.tex
+  sed -i "$linenum i This is sentence $commit_count." foo.tex
 done
-
-
-
-
-
-    # sed -i "$linenum i This is sentence $commit_count." foo.tex
