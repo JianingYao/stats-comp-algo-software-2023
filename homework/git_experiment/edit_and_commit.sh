b561bf3160893c090d2a1046e2a3a3267f6ccedb
JianingYao <jianingyao619@gmail.com>
2023-02-13 15:33:20 -0500

    exercise 1

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -3,5 +3,11 @@
 for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
-  # Fill in
+  sed -i "$linenum i This is the $commit_count th sentence." foo.tex
 done
+
+
+
+
+
+    # sed -i "$linenum i This is sentence $commit_count." foo.tex
