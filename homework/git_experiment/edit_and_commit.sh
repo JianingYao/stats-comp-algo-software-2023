2e32b00726f5cb859d0678f2cb688c09ceab01a3
JianingYao <jianingyao619@gmail.com>
2023-02-13 21:51:53 -0500

    add compiling and git commit in the loop

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+module load tex
+
 for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
   sed -i "$linenum i This is sentence $commit_count." foo.tex
+  pdflatex foo.tex > /dev/null 2>&1
+  git add -u
+  git commit -m "Add another sentence to TeX file"
 done
